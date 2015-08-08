818e82a61665a6dbe5f2f7a8c7d6601538aadd74
Thustra <peter.windey@telenet.be>
2015-08-08 20:42:35 +0200

    default port added

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 __author__ = 'Peter'
 
 from waitress import serve
+import os
 from app import app
 
-serve(app, host='0.0.0.0')
+port = int(os.environ.get('PORT', 9999))
+serve(app, host='0.0.0.0', port=port)
