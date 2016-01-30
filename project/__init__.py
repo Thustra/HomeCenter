84c1d29a087a0c5941389ad2047b0888dba0227f
Thustra <peter.windey@telenet.be>
2016-01-30 21:58:26 +0100

    Added .travis.yml file for CI

--- project/__init__.py
+++ project/__init__.py
@@ -5,8 +5,8 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.bcrypt import Bcrypt
-from flask.ext.login import LoginManager
-from flask_admin import Admin
+from flask.ext.login import LoginManager, current_user
+from flask_admin import Admin, AdminIndexView,expose
 from flask_admin.contrib.sqla import ModelView
 import os
 
@@ -20,7 +20,6 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
-admin = Admin(app,name='HomeCenter')
 
 ##############
 # Blueprints #
@@ -36,19 +35,36 @@ app.register_blueprint(home_blueprint)
 app.register_blueprint(budget_blueprint)
 
 #########
-# Admin #
-#########
-
-
-
-#########
 # Login #
 #########
 
-from project.models import User
+from project.models import *
 
 login_manager.login_view = "users.login"
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.filter(User.id == int(user_id)).first()
+
+#########
+# Admin #
+#########
+
+class MyHomeView(AdminIndexView):
+    @expose('/')
+    def index(self):
+        arg1 = 'Hello'
+        return self.render('admin/myhome.html', arg1=arg1)
+
+admin = Admin(app,name='HomeCenter',index_view=MyHomeView())
+
+class CustomModelView(ModelView):
+
+    def is_accessible(self):
+        print(current_user)
+        return current_user.is_authenticated
+
+admin.add_view(CustomModelView(User, db.session))
+admin.add_view(CustomModelView(Show, db.session))
+admin.add_view(CustomModelView(Expense_category, db.session))
+admin.add_view(CustomModelView(Expense, db.session))
